e1bdf4477c8aaa5cecbe4c4a2c6db12cb3d2da47
sequeirashel <sequeirashel@gmail.com>
2014-04-14 17:36:03 -0700

    Changing approach to slice calculation a bit.

--- src/main/java/com/dyno/core/trace/RWOperation.java
+++ src/main/java/com/dyno/core/trace/RWOperation.java
@@ -7,59 +7,65 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo;
 @JsonTypeInfo(use=JsonTypeInfo.Id.CLASS, include=JsonTypeInfo.As.PROPERTY, property="@class")
 public class RWOperation implements Comparable<RWOperation> {
 
-	private int order;
-	private int lineNo;
-	private String variable;
-	//private int messageType;
-	private RWOperation parentOperation;
-	private ArrayList<RWOperation> childenOperations;
+    private int order;
+    private boolean partOfSlice = false;
+    private int lineNo;
+    private String variable;
+    //private int messageType;
+    private RWOperation parentOperation;
+    private ArrayList<RWOperation> childenOperations;
 
-	public int compareTo(RWOperation arg0) {
-		if (order < arg0.getOrder()) {
-			return -1;
-		} else if (order > arg0.getOrder()) {
-			return 1;
-		}
-		return 0;
-	}
+    public int compareTo(RWOperation arg0) {
+        if (order < arg0.getOrder()) {
+            return -1;
+        } else if (order > arg0.getOrder()) {
+            return 1;
+        }
+        return 0;
+    }
 
-	public int getOrder() {
-		return order;
-	}
+    public int getOrder() {
+        return order;
+    }
+    public void setOrder(int o) {
+        order = o;
+    }
 
-	public void setOrder(int o) {
-		order = o;
-	}
-	
-	public int getLineNo() {
-		return lineNo;
-	}
+    public int getLineNo() {
+        return lineNo;
+    }
+    public void setLineNo(int o) {
+        lineNo = o;
+    }
 
-	public void setLineNo(int o) {
-		lineNo = o;
-	}
-	
-	public String getVariable() {
-		return variable;
-	}
+    public String getVariable() {
+        return variable;
+    }
+    public void setVariable(String o) {
+        variable = o;
+    }
 
-	public void setVariable(String o) {
-		variable = o;
-	}
-	
-	public void addChild (RWOperation c) {
-		this.childenOperations.add(c);
-	}
-	
-	public ArrayList<RWOperation> getChildren() {
-		return this.childenOperations;
-	}
-	
-	public void setParent(RWOperation p) {
-		this.parentOperation = p;
-	}
-	
-	public RWOperation getParent(){
-		return this.parentOperation;
-	}
+    public void addChild (RWOperation c) {
+        this.childenOperations.add(c);
+    }
+    public ArrayList<RWOperation> getChildren() {
+        return this.childenOperations;
+    }
+    public void clearChildren() {
+        this.childenOperations = new ArrayList<RWOperation>();
+    }
+
+    public void setParent(RWOperation p) {
+        this.parentOperation = p;
+    }
+    public RWOperation getParent(){
+        return this.parentOperation;
+    }
+    
+    public void includeInSlice () {
+        this.partOfSlice = true;
+    }
+    public void omitFromSlice () {
+        this.partOfSlice = false;
+    }
 }
